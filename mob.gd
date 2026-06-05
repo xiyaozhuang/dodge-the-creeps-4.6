faf9f48d9ad88c83d05959e1305b6cdef63c786c
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2026-06-05 01:19:02 +0100

    Add mob script

---
+++ mob.gd
@@ -0,0 +1,11 @@
+extends RigidBody2D
+
+
+func _ready() -> void:
+	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
+	$AnimatedSprite2D.animation = mob_types.pick_random()
+	$AnimatedSprite2D.play()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
