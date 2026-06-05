f5107fda8ee48e0cc8fdb5eee1e11bfe2dabb9e8
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2026-06-05 19:58:37 +0100

    Add sound effects

--- main.gd
+++ main.gd
@@ -15,6 +15,7 @@ func new_game() -> void:
 	score = 0
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
@@ -25,6 +26,8 @@ func game_over() -> void:
 	$MobTimer.stop()
 
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 
 func _on_start_timer_timeout() -> void:
