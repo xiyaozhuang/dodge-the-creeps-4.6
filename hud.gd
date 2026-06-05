9612f074c6b9066ec4ca25ffcf640315de471e5f
xiyaozhuang <114228792+xiyaozhuang@users.noreply.github.com>
2026-06-05 19:26:30 +0100

    Add hud scene and script

---
+++ hud.gd
@@ -0,0 +1,34 @@
+extends CanvasLayer
+
+signal start_game
+
+
+func show_message(text) -> void:
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+
+
+func show_game_over() -> void:
+	show_message("Game Over")
+
+	await $MessageTimer.timeout
+
+	$Message.text = "Dodge the Creeps!"
+	$Message.show()
+
+	await get_tree().create_timer(1).timeout
+	$StartButton.show()
+
+
+func update_score(score) -> void:
+	$ScoreLabel.text = str(score)
+
+
+func _on_start_button_pressed() -> void:
+	$StartButton.hide()
+	start_game.emit()
+
+
+func _on_message_timer_timeout() -> void:
+	$Message.hide()
